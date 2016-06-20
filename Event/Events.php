b5afd9d2f309a22c5272bb7404bdf049fc5722c7
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-06-20 17:41:49 +0200

    Clean up events

--- Event/Events.php
+++ Event/Events.php
@@ -8,12 +8,43 @@ namespace Trinity\Bundle\MessagesBundle\Event;
  */
 final class Events
 {
-    const SEND_MESSAGE              = 'trinity.messages.sendMessage';
-    const READ_MESSAGE              = 'trinity.messages.readMessage';
-    const UNPACK_MESSAGE            = 'trinity.messages.unpackMessage';
-    const AFTER_MESSAGE_UNPACKED    = 'trinity.messages.afterUnpackMessage';
-    const READ_MESSAGE_ERROR        = 'trinity.messages.readMessageError';
-    const BEFORE_MESSAGE_PUBLISH    = 'trinity.messages.beforeMessagePublish';
-    const SET_MESSAGE_STATUS        = 'trinity.messages.setMessageStatus';
-    const STATUS_MESSAGE_EVENT      = 'trinity.messages.statusMessageEvent';
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\AfterUnpackMessageEvent")
+     */
+    const AFTER_MESSAGE_UNPACKED = 'trinity.messages.afterUnpackMessage';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\BeforeMessagePublishEvent")
+     */
+    const BEFORE_MESSAGE_PUBLISH = 'trinity.messages.beforeMessagePublish';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\ReadMessageErrorEvent")
+     */
+    const READ_MESSAGE_ERROR     = 'trinity.messages.readMessageError';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\ReadMessageEvent")
+     */
+    const READ_MESSAGE           = 'trinity.messages.readMessage';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\SendMessageEvent")
+     */
+    const SEND_MESSAGE           = 'trinity.messages.sendMessage';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\SetMessageStatusEvent")
+     */
+    const SET_MESSAGE_STATUS     = 'trinity.messages.setMessageStatus';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\StatusMessageEvent")
+     */
+    const STATUS_MESSAGE_EVENT   = 'trinity.messages.statusMessageEvent';
+
+    /**
+     * @Event("Trinity\Bundle\MessagesBundle\Event\UnpackMessageEvent")
+     */
+    const UNPACK_MESSAGE         = 'trinity.messages.unpackMessage';
 }
